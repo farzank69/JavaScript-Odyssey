9fbefd1f627ff3c4069a45886a03f5bfeea17bfb
farzank69 <91688069+farzank69@users.noreply.github.com>
2024-06-21 04:54:43 +0000

    arrys basics

---
+++ 02_basics/arrays.js
@@ -0,0 +1,8 @@
+//  ARRAY             => shallow copy and deep copy
+
+const myArr = [0, 1, 3, 4, 5, 8]
+const myGame = ["clash of clans", "mini militia", "god of war"]
+
+const myGames = new Array(1, 2, 5, 7)
+console.log(myGame[2]);
+
