40321300dcb6d2ad0e0c8a4244a53d4669d3a789
farzank69 <91688069+farzank69@users.noreply.github.com>
2024-06-23 15:17:56 +0000

    de-structure of object and json overview

---
+++ 02_basics/object_de-structure.js
@@ -0,0 +1,38 @@
+//  De-structure
+
+const channel = {
+    courseInstructor: "chai aur code",
+    course: "javascript beginner",
+    price: "free"
+}
+
+// channel.name
+
+const {courseInstructor: instruct} = channel
+
+// console.log(courseInstructor);
+console.log(instruct);
+
+//below is props      it's react 
+// const navbar = ({company}) => {              // { this is the destructuring}
+
+// }
+// navbar(company = "farzan")
+
+
+// JSON format idea
+
+// {
+//     "name" : "farzan",
+//     "location" : "punjab",
+//     "degree" : "btech"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+//. explore randomuserme api to know more about json and api
+//  use json formatter to get easy understanding with the api's.
