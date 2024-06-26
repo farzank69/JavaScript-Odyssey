6e342d291f0b84ec67bc69f8de21df4e40ea6af9
farzank69 <91688069+farzank69@users.noreply.github.com>
2024-06-26 06:01:54 +0000

    while/do-while

---
+++ 05_iterations/prep2.js
@@ -0,0 +1,22 @@
+// while loops
+
+// let index = 0;
+// while (index < 10) {
+//     console.log(`Value is : ${index}`);
+//     index = index + 2;
+// }
+
+const myArray = ['ironman', 'hulk', 'thor']
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is: ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+// do while
+
+let score = 1
+do {
+    console.log(`score is : ${score}`);
+    score++
+} while (score <= 10);
