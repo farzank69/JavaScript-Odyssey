37d7de77d8c718f0651724c00f127ca64027091d
Farzan Khan <farzankhan61r@gmail.com>
2025-10-28 00:01:41 +0530

    functions in ts

---
+++ Section-09/functions.js
@@ -0,0 +1,14 @@
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+addTwo(5);
+getUpper('farzan');
+signUpUser("farzan", "farzan@gmail.com", false);
+loginUser("khan", "khan@example.com");
---
+++ Section-09/functions.ts
@@ -0,0 +1,16 @@
+function addTwo(num:number){
+    return num + 2
+}
+
+function getUpper(val: string){
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean){}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+addTwo(5);
+getUpper('farzan');
+signUpUser("farzan","farzan@gmail.com",false);
+loginUser("khan", "khan@example.com");
