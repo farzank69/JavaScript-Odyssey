edc09c3e3066d6275d6b94468c7dd4fb910b3e57
Farzan Khan <farzankhan61r@gmail.com>
2025-10-28 00:14:16 +0530

    more about functions

--- Section-09/functions.ts
+++ Section-09/functions.ts
@@ -1,5 +1,6 @@
-function addTwo(num:number){
+function addTwo(num:number): number{         // . after function means function can only output number.
     return num + 2
+    // return "hello"
 }
 
 function getUpper(val: string){
@@ -14,3 +15,30 @@ addTwo(5);
 getUpper('farzan');
 signUpUser("farzan","farzan@gmail.com",false);
 loginUser("khan", "khan@example.com");
+
+// Ways of writing functions.
+// function getValue(myVal: number): boolean{
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+const getHello = (s: string): string => {
+    return ""
+}
+
+const heros = ["thor", "spiderman", "batman"]
+heros.map((hero): string => {       // here defining that output should always be a string.
+    return `hero is ${hero}`
+})
+
+function consoleError(errmsg: string): void{
+    console.log(errmsg);
+    // return 1      this will throw error as output is void; it won't return anything.
+}
+
+function handleError(errmsg: string): never{
+    throw new Error(errmsg);
+}
+
+export {}
