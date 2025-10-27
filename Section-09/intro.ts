83e4010bb435ce99f30314df600db31c1b8f4c0d
Farzan Khan <farzankhan61r@gmail.com>
2025-10-27 23:17:59 +0530

    variables typescript

--- Section-09/intro.ts
+++ Section-09/intro.ts
@@ -2,3 +2,18 @@ const user = {name:"Farzan", city:"New Delhi"}
 
 console.log(user.name)
 console.log(user.email)    // an error over here but not in javascript
+
+
+// Types:
+/*
+Number
+String
+Boolean
+Null
+Undefined
+Void
+Object 
+Array
+Tuples ... 
+Any, Never, Unknown -> read documentation
+*/
---
+++ Section-09/variable.ts
@@ -0,0 +1,8 @@
+let greetings: string = "Hello TypeScript"
+let mynum = 88
+
+greetings.toUpperCase()
+
+console.log(greetings);
+
+export {}
