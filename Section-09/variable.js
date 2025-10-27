42cca1eedb94e0b4425e6f2604a35ce4d36bee8b
Farzan Khan <farzankhan61r@gmail.com>
2025-10-27 23:28:53 +0530

    'any' type in ts

--- Section-09/variable.js
+++ Section-09/variable.js
@@ -1,4 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello TypeScript";
+var mynum = 88;
+greetings.toUpperCase();
 console.log(greetings);
+// number
+var userId = 33566.44;
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
--- Section-09/variable.ts
+++ Section-09/variable.ts
@@ -2,7 +2,22 @@ let greetings: string = "Hello TypeScript"
 let mynum = 88
 
 greetings.toUpperCase()
-
 console.log(greetings);
+
+// number
+
+let userId = 33566.44
+userId.toFixed()
+
+// boolean
+let isLoggedIn: boolean = false
+
+let game: string;
+
+function getGame(){
+    return "Red Dead Redemption 2"
+}
+game = getGame()
+
 
 export {}
