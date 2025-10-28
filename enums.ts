a75a1e18c630e2c929ecfec0883afa3592bd2195
Farzan Khan <farzankhan61r@gmail.com>
2025-10-28 17:30:15 +0530

    enums in ts

---
+++ enums.ts
@@ -0,0 +1,7 @@
+enum seatChoice {
+    AISLE = "aisle",           // there are lot more thing in enum to tweak the values.
+    MIDDLE = 7,   
+    WINDOW,
+    FOURTH
+}
+const myseat = seatChoice.AISLE
