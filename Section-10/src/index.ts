4a3f3002aba778847ca4e41d99e82beb0464aff5
Farzan Khan <farzankhan61r@gmail.com>
2025-10-29 12:15:49 +0530

    getters & setters in ts

--- Section-10/src/index.ts
+++ Section-10/src/index.ts
@@ -9,12 +9,35 @@
 // }
 
 class User {                       // Better way of using classes along with its property.
+
+    private _courseCount = 1
+
+    readonly city: string = "Delhi"
     constructor(
         public email: string, 
         public name: string,
         // private userId: string
         ){    
     }
+    get getApplEmail(): string {
+        return `apple${this.email}`
+    }
+    // get is used to get any property (mostly so private methods can be exposed outside with additional logic/restriction)
+    get courseCount(): number {
+        return this._courseCount
+    }
+    // to set the property you need to get it first
+    set courseCount(courseNum) {        //setter: there should be nothing in the return type 
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1!!!")
+        }
+        this.courseCount = courseNum
+    }
 }
+
+
+
+
+
 const farzan = new User("farzan@gmail.com", "farzan");
 // farzan.city = "Saint Denis"
