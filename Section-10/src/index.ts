ac7cc9a8c70c350459d416e8029adc2a3663b58b
Farzan Khan <farzankhan61r@gmail.com>
2025-10-29 12:22:26 +0530

    protected in ts

--- Section-10/src/index.ts
+++ Section-10/src/index.ts
@@ -10,7 +10,8 @@
 
 class User {                       // Better way of using classes along with its property.
 
-    private _courseCount = 1
+    // private _courseCount = 1       // private can accessed within the class not outside 
+    protected _courseCount = 1       // protected can be accessed in the child class but not outside 
 
     readonly city: string = "Delhi"
     constructor(
@@ -19,6 +20,11 @@ class User {                       // Better way of using classes along with its
         // private userId: string
         ){    
     }
+    private deleteToken() {
+        console.log("Token deleted");
+        
+    }
+
     get getApplEmail(): string {
         return `apple${this.email}`
     }
@@ -35,9 +41,17 @@ class User {                       // Better way of using classes along with its
     }
 }
 
+// Inheritence
 
+class subUser extends User {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
 
 
 
 const farzan = new User("farzan@gmail.com", "farzan");
 // farzan.city = "Saint Denis"
+// farzan.deleteToken()
