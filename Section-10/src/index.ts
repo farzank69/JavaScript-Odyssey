2e9ea737371d252494dc070e965f55148101efe3
Farzan Khan <farzankhan61r@gmail.com>
2025-10-29 12:03:54 +0530

    classes public/private in ts

--- Section-10/src/index.ts
+++ Section-10/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Typescript is working!!!")
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Delhi"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+
+class User {                       // Better way of using classes along with its property.
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+        ){    
+    }
+}
+const farzan = new User("farzan@gmail.com", "farzan");
+// farzan.city = "Saint Denis"
