c6a821f39d26836add1d84732eb90b01b251fc59
Farzan Khan <farzankhan61r@gmail.com>
2025-12-05 02:25:49 +0530

    blocking & non-blocking

--- Section-12 NodeJs/fs/file.js
+++ Section-12 NodeJs/fs/file.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
+const os = require("os");
+// fs.writeFileSync('test.txt', 'Hey There!')
 
-// fs.writeFileSync('./Section-12\ NodeJs/fs/test.txt', 'Hey There!')
 
+// fs.appendFileSync('test.txt', 'Hello, Its from async!!')
 
-// fs.appendFileSync('./Section-12\ NodeJs/fs/test.txt', 'Hello, Its from async!!')
+// const result = fs.readFileSync("test.txt", "utf-8")
+// console.log(result)
 
-const result = fs.readFileSync("./Section-12\ NodeJs/fs/test.txt", "utf-8")
-console.log(result)
+fs.readFile('test.txt', 'utf-8', (err, result) => {
+    console.log(result);
+})
+
+// console.log(os.cpus().length);
