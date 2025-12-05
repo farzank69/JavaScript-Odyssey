5a3fa110ce5e23d3c3e0e56da4d3a55c1eee7032
Farzan Khan <farzankhan61r@gmail.com>
2025-12-06 03:32:05 +0530

    use express

---
+++ Section-12 NodeJs/fs/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    return res.send('Hello, This is Home Page')
+});
+
+app.get('/about', (req, res) =>{
+    return res.send(`Hello ${req.query.name}`)
+});
+
+app.listen(8000, () => console.log("Server Started!"));
