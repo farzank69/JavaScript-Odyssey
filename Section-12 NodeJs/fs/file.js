7ce3606e2a9fcce7558ec7fd959823f28e8d0ce0
Farzan Khan <farzankhan61r@gmail.com>
2025-12-09 23:50:22 +0530

    async file handle

--- Section-12 NodeJs/fs/file.js
+++ Section-12 NodeJs/fs/file.js
@@ -12,4 +12,16 @@ fs.readFile('test.txt', 'utf-8', (err, result) => {
     console.log(result);
 })
 
+// async function readFile() {
+//     try {
+//         const data = await fs.promises.readFile('test.txt', 'utf-8');
+
+// (async () => {
+//   const file = await open('./some/file/to/read');
+
+//   for await (const line of file.readLines()) {
+//     console.log(line);
+//   }
+//   })();
+
 // console.log(os.cpus().length);
