bbf768112dfdd56bed3e2957d49a1ade2a7dd859
Farzan Khan <farzankhan61r@gmail.com>
2025-12-20 21:51:50 +0530

    error handling

---
+++ Section-09/error_handlind.ts
@@ -0,0 +1,13 @@
+function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error('Division by zero');
+  }
+  return a / b;
+}
+
+try {
+  const result = divide(10, 0);
+  console.log(result);
+} catch (error: any) {
+  console.error('An error occurred:', error.message);
+}
