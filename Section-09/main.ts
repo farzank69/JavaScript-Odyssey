da109f8d6a3c2016f938646d10ad89c062ce0b1d
Farzan Khan <farzankhan61r@gmail.com>
2025-12-24 01:23:50 +0530

    interface in ts

--- Section-09/main.ts
+++ Section-09/main.ts
@@ -2,6 +2,18 @@ function greet(name: string): string {
     return `Hello, ${name}!`;
 } 
 
+function todaysWeather(): {date: Date, weather: string}{
+    return {date: new Date(), weather: "sunny"}
+}       
+
+interface Weather {
+    date: Date;
+    weather: string;
+}
+
+let weatherToday = todaysWeather();
+console.log(`Date: ${weatherToday.date}, Weather: ${weatherToday.weather}`);
+
 console.log(greet("World"));
 
 export {}
