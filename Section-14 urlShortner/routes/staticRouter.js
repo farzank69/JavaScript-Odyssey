9c9a639690c2688f83eb2691d18b8eecccec1047
Farzan Khan <farzankhan61r@gmail.com>
2026-01-09 00:58:46 +0530

    refactor routes for authorization

--- Section-14 urlShortner/routes/staticRouter.js
+++ Section-14 urlShortner/routes/staticRouter.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const { handleGetAllUrls } = require("../controller/urls");
 const { restrictTo } = require("../middlewares/auth");
+const URL = require("../models/url");
 const router = express.Router();
 
+router.get("/admin/urls", restrictTo(["ADMIN"]), async(req, res) => {
+    const allUrls = await URL.find({});
+    return res.render("home", {
+        urls: allUrls,
+    });
+});
+
 // static route to render the home page
-router.get("/", restrictTo(["NORMAL"]), handleGetAllUrls);
+router.get("/", restrictTo(["NORMAL", "ADMIN"]), handleGetAllUrls);
 
 router.get("/signup", (req, res) => {
     return res.render("signup");
