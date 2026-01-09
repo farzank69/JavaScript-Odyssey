5cddb68e0fc3f068a0afdd89bc7d9e7fa7076ea5
Farzan Khan <farzankhan61r@gmail.com>
2026-01-09 23:57:01 +0530

    setup bot using the client id for interaction

---
+++ Section-15/discord-bot/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config({ quiet: true });
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("messageCreate", (message) => {
+    if(message.author.bot) return;
+  message.reply({
+    content: "Hello from your bot!" 
+  });
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
