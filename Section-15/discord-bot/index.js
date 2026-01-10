daa686bfe6b80fabbee90a62ebda56e236d16fef
Farzan Khan <farzankhan61r@gmail.com>
2026-01-10 14:47:24 +0530

    add custom commands to discord bot

--- Section-15/discord-bot/index.js
+++ Section-15/discord-bot/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config({ quiet: true });
 const { Client, GatewayIntentBits } = require("discord.js");
 
+
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -15,5 +17,9 @@ client.on("messageCreate", (message) => {
     content: "Hello from your bot!" 
   });
 });
+
+client.on("interactionCreate", (interaction) => {
+  interaction.reply("Pong!!")
+})
 
 client.login(process.env.DISCORD_BOT_TOKEN);
